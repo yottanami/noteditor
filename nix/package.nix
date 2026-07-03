fdcdefffbbb8099cb48fb9dffd53d1cdb2750c56
yottanami <yottanami@gnu.org>
2026-07-04 00:44:50 +0200

    Update xfce power manager name

--- nix/package.nix
+++ nix/package.nix
@@ -49,7 +49,7 @@ let
     flameshot
     openssh
     pkgs.dunst # notification daemon the WM routes alerts through
-    pkgs.xfce.xfce4-power-manager # power manager: lid, brightness keys, battery warnings
+    pkgs.xfce4-power-manager # power manager: lid, brightness keys, battery warnings
     pkgs.snixembed # bridges StatusNotifier/AppIndicator icons to XEmbed so exwm-systemtray can show them
     pkgs.networkmanagerapplet # provides `nm-applet` (network tray applet)
     pkgs.blueman # provides `blueman-applet` (bluetooth tray applet)
